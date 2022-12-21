62819385b48eed4b50bda43edab1614756c8cc86
Alfredo20164920 <apaz20164920@gmail.com>
2022-12-20 22:26:46 -0600

    Create function to encrypt
    - Create a functtion to delete accents
    - Create a function to delete number, change lo lower case and delete special characters

---
+++ js/index.js
@@ -0,0 +1,51 @@
+const text = document.getElementById('text');
+
+//buttons
+const encryptBtn = document.getElementById('encrypt');
+const decryptBtn = document.getElementById('decrypt');
+
+// Show the result
+const result = document.getElementById('result');
+
+// Other variables
+const aside = document.getElementsByClassName('aside_previus');
+
+// Event listeners
+encryptBtn.addEventListener('click', () => {
+    
+    let textValue = normalizeText(text.value);
+
+    aside[0].style.display = 'none';
+    result.style.display = 'block';
+
+    result.innerText = encrypt(textValue);
+
+    encrypt(textValue);
+    
+
+});
+
+// Specials functions
+function removeAccents(str) {
+    return str.normalize('NFD').replace(/[\u0300-\u036f]/g, '');
+}
+
+function normalizeText(str) {
+    /**
+     * Remove accents
+     * Convert to lowercase
+     * Remove special characters
+     * Remove numbers
+     */
+    return removeAccents(str.toLowerCase()).replace(/[^a-zA-Z0-9 ]/g, '').replace(/[0-9]/g, '');;
+}
+
+// Main functins
+function encrypt(str) {
+    return str
+        .replace(/e/g, 'enter')
+        .replace(/i/g, 'imes')
+        .replace(/a/g, 'ai')
+        .replace(/o/g, 'ober')
+        .replace(/u/g, 'ufat');
+}
